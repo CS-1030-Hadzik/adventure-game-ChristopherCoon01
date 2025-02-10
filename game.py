c53332535e24f3ec70772db645b3c3391bc30565
Christopher Coon <christophercoon@mail.weber.edu>
2025-02-09 22:54:42 -0700

    ADD: player name, starting area, question, answers with results.

--- game.py
+++ game.py
@@ -16,5 +16,28 @@ print("Your journey begins here...")
 player_name = input ('What is your name, adventurer?')
 
 # Concatenate strings to create a personalized message.
-
 print("Welcome, " + player_name + "! Your journey begins here.")
+
+# Use an f-string to display the message in a more readable way.
+print(f"Welcome, {player_name}! Your journey begins here.")
+
+# Describe the starting area
+starting_area = """
+You find yourself in a dark forest.
+The sound of rustling leaves fills the air.
+A faint path lies ahead, leading deeper into the unknown...
+"""
+print(starting_area)
+
+# Ask the player to make their first descision
+decision = input("Do you wish to take the path? (Yes or No) ") .lower()
+
+# Response based on the player's decision
+if decision == "yes":
+    print(f"Brave choice, {player_name}! You step onto the path and vetnture onward!")
+
+elif decision == "no":
+    print(f"{player_name}, you decide to wait. Perhaps courage will find you later.")
+
+else:
+    print("Confused, you stand still, unsure what to do.")
